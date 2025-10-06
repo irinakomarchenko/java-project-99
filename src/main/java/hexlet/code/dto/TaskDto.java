4295abcd4de4182bf4a3d46d6e0281834c2b0e7a
Irina Komarchenko <irinakomarchenko@gmail.com>
2025-10-06 21:49:21 +0200

    Fixed TaskDto

--- src/main/java/hexlet/code/dto/TaskDto.java
+++ src/main/java/hexlet/code/dto/TaskDto.java
@@ -12,6 +12,6 @@ public class TaskDto {
     private Long statusId;
     private Long assigneeId;
     private Set<Long> labelIds;
-    private TaskStatusDto status;
+    private String status;
 
 }
