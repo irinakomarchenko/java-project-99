128ef9ba5bd56861e9cdae9f07ab6e02d6eb22c7
Irina Komarchenko <irinakomarchenko@gmail.com>
2025-10-06 14:12:22 +0200

    Fixed Task

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -12,8 +12,6 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
